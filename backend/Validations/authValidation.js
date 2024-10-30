a39912fd0ceb4b88423067c1778f529a98f40546
SanjayKumar <0508sanjaykumar@gmail.com>
2024-10-30 18:06:16 +0530

    Format for Sigin Login added

---
+++ backend/Validations/authValidation.js
@@ -0,0 +1,14 @@
+const {z} = require('zod')
+
+const signupSchema = z.object({
+    username: z.string().min(3,"Username should be at least 3 characters long"),
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(6, "Password should be at least 6 characters long")
+})
+
+const loginSchema = z.object({
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(6, "Password should be at least 6 characters long")
+});
+
+module.exports = {signupSchema,loginSchema}
