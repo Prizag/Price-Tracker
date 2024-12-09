44d50be28c869a5e886fc193582937a02ce19314
SanjayKumar <0508sanjaykumar@gmail.com>
2024-12-09 11:59:04 +0530

    Search Route file added

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { signInUser, LogInUser } = require('../controllers/authController');
-
 const router = express.Router();
 
 router.post('/signup', signInUser);
 router.post('/login', LogInUser);
+
+
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const authRoutes = require('./Routes/authRoutes');
+const searchRoutes = require('./Routes/searchRoutes')
 connectDB()
 const app = express()
 const PORT = 3000
@@ -9,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRoutes);
-
+app.use('/search',searchRoutes)
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
