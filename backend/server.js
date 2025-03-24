148130e5378b1a39bc59dbcb4f5a9c1d4dd7f5fb
Priyanka Agarwal <priyanka060905@gmail.com>
2025-03-24 21:28:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 const app = express()
 
 // const generateToken = require('./utils/generateToken');
-const PORT = 3000
+const PORT = process.env.PORT||3000;
 app.use(cors())
 app.use(express.json())
 
